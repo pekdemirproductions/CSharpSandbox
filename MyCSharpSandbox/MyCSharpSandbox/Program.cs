92391506f4d13ac77ec14083fdb72e1724cf0884
Onur Pekdemir <onur.pekdemir@gmail.com>
2017-02-12 16:15:36 +0100

    Added intro and outro methods to program.cs ...

--- MyCSharpSandbox/MyCSharpSandbox/Program.cs
+++ MyCSharpSandbox/MyCSharpSandbox/Program.cs
@@ -12,6 +12,21 @@ namespace MyCSharpSandbox
         {
             // Cleaned up 2017-02-12
 
+            Intro("Welcome. Don't Panic!");
+
+            Outro("Goodbye.");
+
+        }
+
+        public static void Intro(string message)
+        {
+            Console.WriteLine(message + "\n");
+        }
+        public static void Outro(string message)
+        {
+            Console.WriteLine(message + "\n");
+            Console.WriteLine("(Press Enter to terminate program.)");
+            Console.ReadLine();
         }
     }
 }
